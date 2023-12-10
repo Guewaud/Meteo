054dab95389cda39d7814a53c476cd0eb551a4e3
gosht78 <idrissenaciri01@gmail.com>
2023-12-10 18:31:09 +0100

    Update routeur.js

--- page/routeur.js
+++ page/routeur.js
@@ -1,2 +1,10 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import prevision from "./page/prevision"
+import react from 'react';
+import App from '../App';
+
+reactDOM.render(
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+);
