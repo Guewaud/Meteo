632cf3c1e5d14fcfe9ce35c6b8e9978cbe94a54f
Guewaud <103487430+Guewaud@users.noreply.github.com>
2023-12-18 14:22:22 +0100

    Jours suivants

--- components/Forecasts.js
+++ components/Forecasts.js
@@ -17,7 +17,21 @@ export default function Forecasts({ data }){
                 name: format(dt, "EEEE", { locale : fr})
             })
         })
-        setForecasts(forecatsData)
+
+        let newForcastsData = forecatsData.map(forecast => {
+            return forecast.name
+        }).filter((day, index, self) => {
+            return self.indexOf(day) === index
+        }).map((day) => {
+            return{
+                day,
+                data: forecatsData.filter((forecast) => forecast.name === day)
+            }
+        })
+
+        console.log(newForcastsData)
+
+        setForecasts(newForcastsData)
     }, [data])
     return(
         <ScrollView
@@ -27,8 +41,10 @@ export default function Forecasts({ data }){
         >
             {forecasts.map(f => (
                 <View>
-                    <Text>{f.name}</Text>
-                    <Weather forecast={f} />
+                    <Text style={styles.day}>{f.day.toUpperCase()}</Text>
+                    <View style={styles.container}>
+                        {f.data.map(w => <Weather forecast={w} />)}
+                    </View>
                 </View>
             ))}
         </ScrollView>
@@ -39,5 +55,16 @@ const styles = StyleSheet.create({
     scroll:{
         width: "100%",
         height: "35%"
+    },
+    day:{
+        fontSize: 16,
+        fontWeight: "bold",
+        marginBottom: 10,
+        marginLeft: 5
+    },
+    container:{
+        flexDirection:"row",
+        marginLeft: 5,
+        marginRight: 15
     }
 })
